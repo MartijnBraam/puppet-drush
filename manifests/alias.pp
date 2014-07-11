59a8129f69202b6d3f2edc9aeb258531cce82ee0
Martijn Braam <martijn@brixit.nl>
2014-07-11 16:47:43 +0200

    Added basic alias support

---
+++ manifests/alias.pp
@@ -0,0 +1,41 @@
+class aliasfile (
+  $filename = $title,
+  $owner    = 'root',
+  $group    = 'root',
+  $mode     = '0644',
+  ){
+
+  concat{ $filename:
+    mode   => $mode,
+    owner  => $owner,
+    group  => $group,
+  }
+
+  concat::fragment { 'aliasfile_header_${filename}':
+    target   => $filename,
+    content  => "<?php\n\n",
+    order    => "01",
+  }
+
+}
+
+define aliasfile::alias(
+  $alias         = $name,
+  $aliasfile     = "",
+  $url           = "",
+  $root          = "/",
+  $database_host = "localhost",
+  $database_name = "drupal",
+  $database_user = "root",
+  $database_pass = "root",
+  $database_port = "3306",
+  $database_prefix = "",
+) {
+
+  concat::fragment{ "alias_${alias}_${aliasfile}"
+    target => $aliasfile,
+    content => template("drush/alias.erb"),
+    order  => "02",
+  }
+
+}
